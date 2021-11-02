3a4af4cb5644661b9b3a7715c0bba32f5c4f9745
ElliotWride <93586758+ElliotWride@users.noreply.github.com>
2021-11-02 19:18:01 +0000

    Add files via upload

--- covid dashboard.py
+++ covid dashboard.py
@@ -9,8 +9,12 @@ def test_parse_csv_data ():
 
 def test_process_covid_csv_data ():
     last7days_cases, current_hospital_cases, total_deaths = process_covid_csv_data(parse_csv_data('nation_2021-10-28.csv'))
-    assert last7days_cases == 240_299
-    assert current_hospital_cases == 7_019
-    assert total_deaths == 141_544
+    print(current_hospital_cases)
+    print(last7days_cases)
+    assert last7days_cases == 240299
+    assert current_hospital_cases == 7019
+    assert total_deaths == 141544
+    
 
 test_process_covid_csv_data()
+#test_parse_csv_data()
