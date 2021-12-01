fb6eba5c5916204498818ebd70fea8308fd7c288
ElliotWride <93586758+ElliotWride@users.noreply.github.com>
2021-12-01 16:43:44 +0000

    Add files via upload

--- covid_news.py
+++ covid_news.py
@@ -1,7 +1,8 @@
 import requests
 import json
 
-def news_API_request(covid_terms = "Covid COVID_19 coronavirus"):
+def news_API_request(covid_terms = "Covid COVID_19 coronavirus", removed_articles = []):
+    
     with open('config.json') as json_file:
         config = json.load(json_file)
     api_key = config['api_key']
@@ -9,10 +10,20 @@ def news_API_request(covid_terms = "Covid COVID_19 coronavirus"):
     query = ""
     for word in covid_terms:
         query = "q="+word+"&"+query
-    url = ('https://newsapi.org/v2/top-headlines?'
+    url = ('https://newsapi.org/v2/everything?'
            +query+
            'sortBy=popularity&'
            'language = en&'
            'apiKey='+api_key)
-    return (requests.get(url).json())
+    index = 0
+    news_json = requests.get(url).json()['articles']
+    
+    if removed_articles != []:
+        for article in news_json:
+            for removed_article in removed_articles:
+                if article == removed_article:
+                    news_json.pop(index)
+        
+    return (news_json)
+
 
